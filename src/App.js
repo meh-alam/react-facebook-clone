0da9b6a7e349cbfcd3531c966a9f14a00859f88c
mehtab alam <mehtab@zenKoders.com>
2021-12-10 19:17:41 +0500

    done with profile page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './pages/home/Home'
+import Profile from './pages/profile/profile'
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      <Profile/>
     </div>
   );
 }
